b18414e64bef2387dd8b29e9531bf0f25d55f5cf
troy <gordontaaj@gmail.com>
2019-01-25 21:56:54 -0800

    finished

---
+++ client/build/precache-manifest.c02c77c624faa1534a5201d259f5e8ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "addd0dd36016a58c9829",
+    "url": "/static/js/main.addd0dd3.chunk.js"
+  },
+  {
+    "revision": "9840d1190ff12a1fa396",
+    "url": "/static/js/1.9840d119.chunk.js"
+  },
+  {
+    "revision": "addd0dd36016a58c9829",
+    "url": "/static/css/main.c5c49686.chunk.css"
+  },
+  {
+    "revision": "88595d337be2fe5ada2a21699952192d",
+    "url": "/index.html"
+  }
+];
---
+++ client/config/index.js
@@ -0,0 +1,3 @@
+const MONGODB_URI = 'mongodb://localhost:27017/books';
+
+module.exports = MONGODB_URI;
